d8caaf3dde33bd42b9412809080aa22db15fd93e
Valentin Grtz <39189263+ValentinGratz@users.noreply.github.com>
2023-09-17 13:04:58 +0200

    Add files via upload

---
+++ download-icon-woocommerce.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Téléchargement Icône WooCommerce
+Description: Ajoute une icône de téléchargement pour les produits téléchargeables.
+Version: 1.0.1
+Author: Votre Nom
+*/
+
+function custom_add_download_icon() {
+    // Code pour ajouter l'icône de téléchargement
+    function custom_add_download_icon() {
+    if (is_product() || is_shop()) { // Vérifiez si c'est une page de produit ou de boutique
+        echo '<div class="download-icon"><img src="download-solid.png" alt="Telechargement"></div>';
+    }
+}
+
+}
+add_action('plugins_loaded', 'custom_add_download_icon');
