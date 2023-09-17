5091b539a59e389cbe9a2749849d0f1604845980
Valentin Grtz <39189263+ValentinGratz@users.noreply.github.com>
2023-09-17 13:28:34 +0200

    code corrigé

--- download-icon-woocommerce.php
+++ download-icon-woocommerce.php
@@ -8,7 +8,6 @@ Author: Votre Nom
 
 function custom_add_download_icon() {
     // Code pour ajouter l'icône de téléchargement
-    function custom_add_download_icon() {
     if (is_product() || is_shop()) { // Vérifiez si c'est une page de produit ou de boutique
         echo '<div class="download-icon"><img src="download-solid.png" alt="Telechargement"></div>';
     }
