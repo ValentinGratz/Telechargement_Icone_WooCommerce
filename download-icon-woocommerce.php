606645f2a7687e1b315c284b15738a289d51183c
Valentin Grtz <39189263+ValentinGratz@users.noreply.github.com>
2023-09-17 13:42:07 +0200

    Update download-icon-woocommerce.php
    - Ajout de styles CSS 
    - Gestion des produits téléchargeables : code d'icône est encapsulé dans cette condition

--- download-icon-woocommerce.php
+++ download-icon-woocommerce.php
@@ -7,10 +7,16 @@ Author: Valentin Grätz
 */
 
 function custom_add_download_icon() {
-    // Code pour ajouter l'icône de téléchargement
     if (is_product() || is_shop()) { // Vérifiez si c'est une page de produit ou de boutique
-        echo '<div class="download-icon"><img src="download-solid.png" alt="Telechargement"></div>';
+        echo '<div class="download-icon"><img src="' . plugin_dir_url(__FILE__) . 'images/download-icon.png" alt="Téléchargement"></div>';
+    }
 }
+add_action('woocommerce_before_shop_loop_item_title', 'custom_add_download_icon');
+add_action('woocommerce_before_single_product_summary', 'custom_add_download_icon');
 
+function custom_add_styles() {
+    if (is_product() || is_shop()) { // Vérifiez si c'est une page de produit ou de boutique
+        wp_enqueue_style('custom-download-icon-css', plugin_dir_url(__FILE__) . 'css/download-icon.css');
+    }
 }
-add_action('plugins_loaded', 'custom_add_download_icon');
+add_action('wp_enqueue_scripts', 'custom_add_styles');
